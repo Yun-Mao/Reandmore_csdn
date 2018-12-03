5bfa54ec07f9358bdb6b4ef608019120d1dee582
“yunmao <china.tianyu.dai@gmail.com>
2018-12-03 14:27:50 +0800

    基础版本

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Reandmore_csdn
+
+## 关于项目
+
+本项目基于Chrome浏览器插件，对CSDN的阅读更多进行处理。
---
+++ csdn/maidian.js
@@ -0,0 +1,7 @@
+obj = document.getElementById("article_content");
+obj.style.height= ""
+obj.style.overflow= ""
+obj = document.getElementById("btn-readmore");
+obj.style.display   =   "none"; 
+arr = document.getElementsByClassName("hide-article-box text-center");
+arr[0].classList.remove("hide-article-box");
